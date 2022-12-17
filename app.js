0c75227794986021baa7b2fb5e44dfcc06797ef1
JonathanVillela <villela.jonathan.m@gmail.com>
2022-12-17 15:05:40 -0600

    "async error handling"

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/expressError');
 const methodOverride = require('method-override');
 const Trail = require('./models/trail');
+
 
 mongoose.connect('mongodb://localhost:27017/HikeMate');
 
@@ -26,41 +29,52 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/trails', async (req, res) => {
+app.get('/trails', catchAsync(async (req, res, next) => {
     const trails = await Trail.find({});
     res.render('trails/index', { trails })
-})
+}))
 
 app.get('/trails/new', (req, res) => {
     res.render('trails/new');
 })
 
-app.post('/trails', async (req, res) => {
+app.post('/trails', catchAsync(async (req, res, next) => {
     const trail = new Trail(req.body.trail);
     await trail.save();
     res.redirect(`/trails/${trail._id}`) //directs to new trail
-})
+}))
 
-app.get('/trails/:id', async (req, res) => {
+app.get('/trails/:id', catchAsync(async (req, res, next) => {
     const trail = await Trail.findById(req.params.id)
     res.render('trails/show', { trail });
-})
+}))
 
-app.get('/trails/:id/edit', async (req, res) => {
+app.get('/trails/:id/edit', catchAsync(async (req, res, next) => {
     const trail = await Trail.findById(req.params.id)
     res.render('trails/edit', { trail });
-})
+}))
 
-app.put('/trails/:id', async (req, res) => {
+app.put('/trails/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const trail = await Trail.findByIdAndUpdate(id, { ...req.body.trail });
     res.redirect(`/trails/${trail._id}`)
-})
+}))
 
-app.delete('/trails/:id', async (req, res) => {
+app.delete('/trails/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Trail.findByIdAndDelete(id);
     res.redirect('/trails');
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+// Error Handling: Internal Server Error
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
